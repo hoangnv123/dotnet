7d3b7ae8e4a93d009e854d7181c3f8740d208d41
Ruslan Yuzkiv <55447008+navna@users.noreply.github.com>
2022-02-18 12:11:41 +0400

    Add solution for examples

---
+++ Lecture01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lecture01;
+
+static class Program
+{
+    interface IContext
+    {
+        int GetContext();
+    }
+
+    class BaseWindow : IContext
+    {
+        public int GetContext() => 3;
+    }
+
+    class DerivedWindow : BaseWindow
+    {
+        public new int GetContext() => 7;
+    }
+
+    static void Main()
+    {
+        IContext baseContext = new BaseWindow();
+        IContext derivedContext = new DerivedWindow();
+        Console.WriteLine(baseContext.GetContext());
+        Console.WriteLine(derivedContext.GetContext());
+    }
+}
