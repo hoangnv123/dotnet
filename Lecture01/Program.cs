b33c099ce004bfe9928ec3324becc67030788d02
Ruslan Yuzkiv <55447008+navna@users.noreply.github.com>
2022-02-18 21:58:21 +0400

    Remove obsolete code

--- Lecture01/Program.cs
+++ Lecture01/Program.cs
@@ -1,29 +1,8 @@
-﻿using System;
+﻿namespace Lecture01;
 
-namespace Lecture01;
-
-static class Program
+internal static class Program
 {
-    interface IContext
+    private static void Main()
     {
-        int GetContext();
-    }
-
-    class BaseWindow : IContext
-    {
-        public int GetContext() => 3;
-    }
-
-    class DerivedWindow : BaseWindow
-    {
-        public new int GetContext() => 7;
-    }
-
-    static void Main()
-    {
-        IContext baseContext = new BaseWindow();
-        IContext derivedContext = new DerivedWindow();
-        Console.WriteLine(baseContext.GetContext());
-        Console.WriteLine(derivedContext.GetContext());
     }
 }
